52ae5fe1b4791e830d50593446743397f98e6c76
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2024-11-25 14:07:23 +0100

    Structure of files
    File show.php have a simple structure

--- developers/index.php
+++ developers/index.php
@@ -12,11 +12,12 @@
         echo '<table style="border: solid 1px black">';
         $select_dev = "select * from developers";
         $dev_res = $conn->query($select_dev);
-        $index = 1;
+        
         if ($dev_res -> num_rows > 0) {
             while ($row = $dev_res -> fetch_assoc()) {
+                $index = $row['ID'];
                 echo '<td>';
-                echo "<a href = './show.php?'.'id=111'.'>'".$row['NAME']."</a>";
+                echo "<a href='./show.php?id=" . $index . "'>" . $row['NAME'] . "</a>";
                 echo '</td>';
             };
         };
---
+++ developers/show.php
@@ -0,0 +1,24 @@
+<?php
+    require ('../conn.php')
+?>
+<!DOCKTYPE HTML>
+<html>
+    <head>
+
+    </head>
+    <body>
+
+    <?php
+        switch ($_GET['ID']) {
+
+            case '1':   
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+        }
+    ?>
+    </body>
+</html>
