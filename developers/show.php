329ce3a673e9d0d968c26ac39a6a9d7f7c97ff23
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2024-12-01 15:22:28 +0100

    DEV and GAME folder
    Code reducted to 25% give or take as well as faster and easier to read

--- developers/show.php
+++ developers/show.php
@@ -9,184 +9,28 @@
     <body>
 
     <?php
-        switch ($_GET['id']) {
+        
 
-            case '1':  
-                $sql = "
-            SELECT 
-                games.TITLE, 
-                tags.name AS TAG, 
-                developers.name AS DEVELOPER, 
-                publishers.name AS PUBLISHER, 
-                platform.name AS PLATFORM
-            FROM 
-                games
-            LEFT JOIN tags ON games.TAG = tags.id
-            LEFT JOIN developers ON games.DEVELOPER = developers.id
-            LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-            LEFT JOIN platform ON games.PLATFORM = platform.id
-            WHERE 
-                games.id = 1;
-                        ";
-                $result = mysqli_query($conn, $sql);
-                if ($result -> num_rows > 0) {
-                    while ($row = $result -> fetch_assoc()) {
-                        echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                        echo '</br>';
-                    };
-                };
-                break;
-            case '2':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
+        $id = $_GET['id'];
+        $sql = "
+                SELECT
+                    g.TITLE TITLE,
+                    g.ID ID,
+                    developers.id DID,
+                    developers.NAME DEVELOPER
+                FROM
+                    games g 
+                LEFT JOIN 
+                    developers ON g.DEVELOPER = developers.id
                 WHERE 
-                    games.id = 2;
-                            ";
+                    developers.id =  '$id' ;";
                     $result = mysqli_query($conn, $sql);
                     if ($result -> num_rows > 0) {
                         while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
+                            echo $row['DEVELOPER'] .' | '. $row['TITLE'];
                             echo '</br>';
-                        };
+                        }
                     };
-                    break;
-            case '3':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 3;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .'  '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '4':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 4;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '5':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 5;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '6':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 6;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '7':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 7;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-        }
     ?>
     </body>
 </html>
--- games/show.php
+++ games/show.php
@@ -9,209 +9,30 @@
     <body>
 
     <?php
-        switch ($_GET['id']) {
+        $id = $_GET['id'];
+        $sql = "
+                SELECT 
+                    games.TITLE, 
+                    tags.name AS TAG, 
+                    developers.name AS DEVELOPER, 
+                    publishers.name AS PUBLISHER, 
+                    platform.name AS PLATFORM
+                FROM 
+                    games
+                LEFT JOIN tags ON games.TAG = tags.id
+                LEFT JOIN developers ON games.DEVELOPER = developers.id
+                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
+                LEFT JOIN platform ON games.PLATFORM = platform.id
+                WHERE 
+                    games.id = '$id' ;";
+                    $result = mysqli_query($conn, $sql);
+                    if ($result -> num_rows > 0) {
+                        while ($row = $result -> fetch_assoc()) {
+                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
+                            echo '</br>';
+                        }
+                    };
 
-            case '1':  
-                $sql = "
-            SELECT 
-                games.TITLE, 
-                tags.name AS TAG, 
-                developers.name AS DEVELOPER, 
-                publishers.name AS PUBLISHER, 
-                platform.name AS PLATFORM
-            FROM 
-                games
-            LEFT JOIN tags ON games.TAG = tags.id
-            LEFT JOIN developers ON games.DEVELOPER = developers.id
-            LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-            LEFT JOIN platform ON games.PLATFORM = platform.id
-            WHERE 
-                games.id = 1;
-                        ";
-                $result = mysqli_query($conn, $sql);
-                if ($result -> num_rows > 0) {
-                    while ($row = $result -> fetch_assoc()) {
-                        echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                        echo '</br>';
-                    };
-                };
-                break;
-            case '2':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 2;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '3':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 3;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .'  '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '4':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 4;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '5':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 5;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '6':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 6;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '7':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 7;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-            case '8':
-                $sql = "
-                SELECT 
-                    games.TITLE, 
-                    tags.name AS TAG, 
-                    developers.name AS DEVELOPER, 
-                    publishers.name AS PUBLISHER, 
-                    platform.name AS PLATFORM
-                FROM 
-                    games
-                LEFT JOIN tags ON games.TAG = tags.id
-                LEFT JOIN developers ON games.DEVELOPER = developers.id
-                LEFT JOIN publishers ON games.PUBLISHER = publishers.id
-                LEFT JOIN platform ON games.PLATFORM = platform.id
-                WHERE 
-                    games.id = 8;
-                            ";
-                    $result = mysqli_query($conn, $sql);
-                    if ($result -> num_rows > 0) {
-                        while ($row = $result -> fetch_assoc()) {
-                            echo $row['TITLE'] .' '. $row['TAG'] .' '. $row['DEVELOPER'] .' '. $row['PUBLISHER'] .' '. $row['PLATFORM'];
-                            echo '</br>';
-                        };
-                    };
-                    break;
-        }
     ?>
     </body>
 </html>
